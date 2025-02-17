9391e4335675b874dc6b6ba8f0cbdffa281138a6
JoshuaDeveloper25 <amandasarvelasquez@gmail.com>
2025-02-17 14:55:27 -0600

    Added all icons of midu project and almost finished nav

---
+++ src/data/data.js
@@ -0,0 +1,21 @@
+export const navLinks = [
+  {
+    href: "#experience",
+    text: "Experiencia",
+  },
+
+  {
+    href: "#projects",
+    text: "Proyectos",
+  },
+
+  {
+    href: "#aboutme",
+    text: "Sobre mí",
+  },
+
+  {
+    href: "#contact",
+    text: "Contacto",
+  },
+];
